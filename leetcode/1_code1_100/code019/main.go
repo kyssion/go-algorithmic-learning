de4847e02a32ac617572d2af9afbaad42f2ad8e0
kyssion <1409915687@qq.com>
2022-04-07 20:07:47 +0800

    feat: 2022/4/7

--- leetcode/1_code1_100/code019/main.go
+++ leetcode/1_code1_100/code019/main.go
@@ -7,7 +7,7 @@ func main() {
 	lib.ShowListNode(removeNthFromEnd(nodelist, 1))
 }
 
-//
+// todo 这里有一个地方 需要注意一下 ， 就是边界问题， i = 0 遍历的时候 = 是n+1 < 是n ， node节点也是这样的
 func removeNthFromEnd(head *lib.ListNode, n int) *lib.ListNode {
 	if n == 0 {
 		return head

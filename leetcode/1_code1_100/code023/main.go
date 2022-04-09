0f521dc4c0eb0809f647634dcd3b4aeb153410c3
kyssion <1409915687@qq.com>
2022-04-09 14:24:43 +0800

    feat: upgrade

--- leetcode/1_code1_100/code023/main.go
+++ leetcode/1_code1_100/code023/main.go
@@ -1,5 +1,62 @@
 package main
 
+import (
+	"container/heap"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
 func main() {
 
+}
+
+type ListsHeap []*ListNode
+
+func (l ListsHeap) Len() int {
+	return len(l)
+}
+
+func (l ListsHeap) Less(i, j int) bool {
+	return l[i].Val < l[j].Val
+}
+
+func (l ListsHeap) Swap(i, j int) {
+	l[i], l[j] = l[j], l[i]
+}
+
+func (l *ListsHeap) Push(x any) {
+	if l == nil {
+		return
+	}
+	*l = append(*l, x.(*ListNode))
+}
+
+func (l *ListsHeap) Pop() any {
+	if l == nil {
+		return nil
+	}
+	old := *l
+	n := len(old)
+	x := old[n-1]
+	*l = old[0 : n-1]
+	return x
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+	h := ListsHeap(lists)
+	heap.Init(&h)
+	ansHeader := &ListNode{}
+	itemH := ansHeader
+	for len(h) > 0 {
+		item := heap.Pop(&h).(*ListNode)
+		itemH.Next = item
+		itemH = itemH.Next
+		if item.Next != nil {
+			heap.Push(&h, item.Next)
+		}
+	}
+	return ansHeader.Next
 }

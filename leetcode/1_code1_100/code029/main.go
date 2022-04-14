374070a1530dedd8a838b5704b697042508cdb79
kyssion <1409915687@qq.com>
2022-04-14 17:37:17 +0800

    f

---
+++ leetcode/1_code1_100/code029/main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+
+}
+
+func divide(dividend int, divisor int) int {
+	if divisor == 1 {
+		return dividend
+	}
+	if divisor > dividend {
+		return 0
+	}
+
+	dendNum := [32]int32{}
+	sorNum := [32]int32{}
+	return 1
+}
